2994c2a24b9ce3d52599d701416444eefacaca03
Pedro Gomes <otherview@gmail.com>
2024-05-28 03:00:07 +0100

    dashboard: remove the dashboard (#20279) (#6)
    * dashboard: remove the dashboard (#20279)
    
    This removes the dashboard project. The dashboard was an experimental
    browser UI for geth which displayed metrics and chain information in
    real time. We are removing it because it has marginal utility and nobody
    on the team can maintain it.
    
    Removing the dashboard removes a lot of dependency code and shaves
    6 MB off the geth binary size.
    
    * tweak and make build pass
    
    ---------
    
    Co-authored-by: Felix Lange <fjl@twurst.com>
    Co-authored-by: tony <liboliqi@gmail.com>

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -63,10 +63,6 @@ var (
 		utils.DataDirFlag,
 		utils.KeyStoreDirFlag,
 		utils.NoUSBFlag,
-		utils.DashboardEnabledFlag,
-		utils.DashboardAddrFlag,
-		utils.DashboardPortFlag,
-		utils.DashboardRefreshFlag,
 		utils.EthashCacheDirFlag,
 		utils.EthashCachesInMemoryFlag,
 		utils.EthashCachesOnDiskFlag,
@@ -207,11 +203,7 @@ func init() {
 	app.Before = func(ctx *cli.Context) error {
 		runtime.GOMAXPROCS(runtime.NumCPU())
 
-		logdir := ""
-		if ctx.GlobalBool(utils.DashboardEnabledFlag.Name) {
-			logdir = (&node.Config{DataDir: utils.MakeDataDir(ctx)}).ResolvePath("logs")
-		}
-		if err := debug.Setup(ctx, logdir); err != nil {
+		if err := debug.Setup(ctx, ""); err != nil {
 			return err
 		}
 		// Cap the cache allowance and tune the garbage collector
--- cmd/geth/usage.go
+++ cmd/geth/usage.go
@@ -22,8 +22,6 @@ import (
 	"io"
 	"sort"
 
-	"strings"
-
 	"github.com/ethereum/go-ethereum/cmd/utils"
 	"github.com/ethereum/go-ethereum/internal/debug"
 	"gopkg.in/urfave/cli.v1"
@@ -101,16 +99,6 @@ var AppHelpFlagGroups = []flagGroup{
 			utils.EthashDatasetsOnDiskFlag,
 		},
 	},
-	//{
-	//	Name: "DASHBOARD",
-	//	Flags: []cli.Flag{
-	//		utils.DashboardEnabledFlag,
-	//		utils.DashboardAddrFlag,
-	//		utils.DashboardPortFlag,
-	//		utils.DashboardRefreshFlag,
-	//		utils.DashboardAssetsFlag,
-	//	},
-	//},
 	{
 		Name: "TRANSACTION POOL",
 		Flags: []cli.Flag{
@@ -302,9 +290,6 @@ func init() {
 			uncategorized := []cli.Flag{}
 			for _, flag := range data.(*cli.App).Flags {
 				if _, ok := categorized[flag.String()]; !ok {
-					if strings.HasPrefix(flag.GetName(), "dashboard") {
-						continue
-					}
 					uncategorized = append(uncategorized, flag)
 				}
 			}
